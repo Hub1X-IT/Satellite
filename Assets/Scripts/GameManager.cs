1118190968968efe693b6928ce9e88e95cbe223d
Widzew12 <szmnglowacz@gmail.com>
2023-10-24 16:57:25 +0200

    Small fixes

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,9 +7,6 @@ public class GameManager : MonoBehaviour
     public static GameManager Instance { get; private set; }
 
     private void Awake() {
-        if (Instance != null) {
-            Debug.LogError("There is more than one GameManager instance");
-        }
         Instance = this;
 
         HideCursor();
--- Assets/Scripts/Lamp.cs
+++ Assets/Scripts/Lamp.cs
@@ -4,20 +4,15 @@ using UnityEngine;
 
 public class Lamp : MonoBehaviour {
 
-    [SerializeField] private int maxOnOffCycles;
-
+    
     [SerializeField] private GameObject[] turnedOnObjects;
     [SerializeField] private GameObject[] turnedOffObjects;
 
 
     private bool isTurnedOn;
     private int onOffCycles;
+    private int maxOnOffCycles = 5;
 
-
-    private void Start() {
-        isTurnedOn = false;
-        onOffCycles = 0;
-    }
 
     private void Update() {
         if (Input.GetKeyDown(KeyCode.F) && onOffCycles < maxOnOffCycles) {
