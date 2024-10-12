73bfbee232f9c1e4c8eb0ce6760c3a5570bb5ff5
Hub1X <hubert.poteraj@gmail.com>
2024-10-12 19:42:59 +0200

    Scripting doors
    - Door functionality not done
    - Inside walls with door frame will be added back soon with doors
    - Gonna need 3 animators...

--- Assets/Scripts/InteractableObjects/Door.cs
+++ Assets/Scripts/InteractableObjects/Door.cs
@@ -4,10 +4,29 @@ using UnityEngine;
 
 public class Door : MonoBehaviour, IInteractable
 {
+    private Animator doorAnimator;
+
+    private const string DOOR_OPEN = "DoorOpen";
+    private const string DOOR_CLOSE = "DoorClose";
+
+    private bool doorOpened = false;
     public Transform GetTransform() { return transform; }
 
+    public void Awake()
+    {
+        doorAnimator = GetComponent<Animator>();
+    }
     public void Interact()
     {
-
+        if (doorOpened)
+        {
+            doorOpened = true;
+            doorAnimator.SetTrigger(DOOR_OPEN);
+        }
+        else
+        {
+            doorOpened = false;
+            doorAnimator.SetTrigger(DOOR_CLOSE);
+        }
     }
 }
