bd010f1903ba0ed732679ba45393200d34d5a0a0
Hub1X <hubert.poteraj@gmail.com>
2024-10-12 13:03:30 +0200

    Further door animation preparations
    - Added door script (not done yet)
    - Added Interaction interface
    - Created animation controller
    - Created open & close animation

---
+++ Assets/Scripts/InteractableObjects/Door.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour, IInteractable
+{
+    public Transform GetTransform() { return transform; }
+
+    public void Interact()
+    {
+
+    }
+}
