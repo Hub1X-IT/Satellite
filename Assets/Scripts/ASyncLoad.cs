2815cf0c9abd039b2b5179c1c76f8721bafa5895
Hub1X <hubert.poteraj@gmail.com>
2024-11-10 14:55:46 +0100

    ASync Scene Loader Done
    - Need to move load function into the Menu script

---
+++ Assets/Scripts/ASyncLoad.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class ASyncLoad : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject loadingScreen;
+
+    [SerializeField]
+    private GameObject mainMenu;
+
+    [SerializeField]
+    private Slider loadingSlider;
+
+    public void LoadLevelButton(string leverToLoad)
+    {
+        mainMenu.SetActive(false);
+        loadingScreen.SetActive(true); 
+
+        //Run the A sync
+        StartCoroutine(LoadLevelAsync(leverToLoad));
+    }
+
+    IEnumerator LoadLevelAsync(string levelToLoad)
+    {
+        AsyncOperation loadOperation = SceneManager.LoadSceneAsync(levelToLoad);
+        
+        while (!loadOperation.isDone)
+        {
+            float progressValue = Mathf.Clamp01(loadOperation.progress / 0.9f);
+            loadingSlider.value = progressValue;
+            yield return null;
+        }
+    }
+}
