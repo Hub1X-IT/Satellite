c90c4f4c975a68bc352725ca39d74c3d725e484a
Widzew12 <szmnglowacz@gmail.com>
2024-11-15 19:43:57 +0100

    Add new game event listener and attach to Player HUD

---
+++ Assets/Scripts/GameEvents/NewGameEventListener.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class NewGameEventListener : MonoBehaviour
+{
+    [SerializeField]
+    private NewGameEventSO[] gameEvents;
+
+    public UnityEvent OnGameEventRaised;
+
+    private void Awake()
+    {
+        foreach (var gameEvent in gameEvents)
+        {
+            gameEvent.EventRaised += InvokeUnityEvent;
+        }
+    }
+
+    /*
+    private void OnEnable()
+    {
+        foreach (var gameEvent in gameEvents)
+        {
+            gameEvent.EventRaised += InvokeUnityEvent;
+        }
+    }
+
+    private void OnDisable()
+    {
+        foreach (var gameEvent in gameEvents)
+        {
+            gameEvent.EventRaised -= InvokeUnityEvent;
+        }
+    }
+    */
+    /*
+    private void OnDestroy()
+    {
+        foreach (var gameEvent in gameEvents)
+        {
+            gameEvent.EventRaised -= InvokeUnityEvent;
+        }
+    }
+    */
+
+    private void InvokeUnityEvent()
+    {
+        OnGameEventRaised?.Invoke();
+    }
+}
