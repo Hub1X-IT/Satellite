8d0ebbb452ced3cdbb2910ff5feb1a9b554a5d5e
Widzew12 <szmnglowacz@gmail.com>
2024-11-17 18:47:02 +0100

    Create objectives UI prefab and prefab variant for level 1

--- Assets/Scripts/UI/Smartphone/SmartphoneUI.cs
+++ Assets/Scripts/UI/Smartphone/SmartphoneUI.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using UnityEngine.UI;
 
-public class SmartphoneControllerUI : MonoBehaviour
+public class SmartphoneUI : MonoBehaviour
 {
     [SerializeField]
     private Animator smartphoneAnimator;
