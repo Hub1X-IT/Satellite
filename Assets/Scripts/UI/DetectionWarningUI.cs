f7100e257ba0b36f7825fc7c05f26b98555b13f3
Hub1X <hubert.poteraj@gmail.com>
2024-12-15 00:26:12 +0100

    Add new sprites and detection warning and cursor

---
+++ Assets/Scripts/UI/DetectionWarningUI.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class DetectionWarningUI : MonoBehaviour
+{
+    [SerializeField]
+    private RectTransform detectionWarning;
+
+    private void ShowHideWarning()
+    {
+        detectionWarning.sizeDelta = DetectionManager.WasDetected ? new Vector2(280, 400) : new Vector2(-280, 400);
+    }
+}
