9a0a0c9383114fe1ca2ef94357628e085d7bf8db
Widzew12 <szmnglowacz@gmail.com>
2025-03-23 13:58:10 +0100

    Fix Guidebook UI script

--- Assets/Scripts/UI/Guidebook/GuidebookUI.cs
+++ Assets/Scripts/UI/Guidebook/GuidebookUI.cs
@@ -1,72 +1,85 @@
-using TMPro;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class GuidebookUI : MonoBehaviour
 {
-    [SerializeField]
-    private Button nextPageButton;
-    [SerializeField]
-    private Button previousPageButton;
-    [SerializeField]
-    private TMP_Text textLeftPage;
-    [SerializeField]
-    private TMP_Text textRightPage;
-
-    private int pageNumber;
+    // Left - even number (starting from 0)
+    // Right - odd number (starting from 1)
 
     [SerializeField]
     private GameObject[] pages;
 
+    // Button index must match the corresponding page index
     [SerializeField]
     private Button[] pageListButtons;
 
+    [SerializeField]
+    private Button nextPageButton;
+    [SerializeField]
+    private Button previousPageButton;
+
+    private int currentPageNumber;
+
+    private Dictionary<Button, int> buttonToPageNumber = new();
+
     private void Awake()
     {
-        foreach (var button in pageListButtons)
+        for (int i = 0; i < pageListButtons.Length; i++)
+        {
+            if (pageListButtons[i] != null)
+            {
+                buttonToPageNumber.Add(pageListButtons[i], i);
+            }
+        }
+
+        foreach (var button in buttonToPageNumber.Keys)
         {
             button.onClick.AddListener(() =>
             {
-            for (int i = 0, y = 0; i < pages.Length && y < pageListButtons.Length; i++, y++)
-                {
-                    if (y == i+1)
-                    {
-                        pages[0].SetActive(false);
-                        pages[1].SetActive(false);
-                        pages[i + 1].gameObject.SetActive(true);
-                        pages[i + 2].gameObject.SetActive(true);
-                        break;
-                    }
-                }
+                ChangePage(buttonToPageNumber[button]);
             });
         }
+
         nextPageButton.onClick.AddListener(() =>
         {
-            for (int i = 0; i < pages.Length; i++)
-            {
-                if (pages[i] == isActiveAndEnabled && pages[i + 1] == isActiveAndEnabled && i < pages.Length + 1)
-                {
-                    pages[i].SetActive(false);
-                    pages[i + 1].SetActive(false);
-                    pages[i + 2].SetActive(true);
-                    pages[i + 3].SetActive(true);
-                    break;
-                }
-            }
+            ChangePage(currentPageNumber + 2);
         });
         previousPageButton.onClick.AddListener(() =>
         {
-            for (int i = 0; i < pages.Length; i++)
-            {
-                if (pages[i] == isActiveAndEnabled && pages[i + 1] == isActiveAndEnabled && i >= 2)
-                {
-                    pages[i].SetActive(false);
-                    pages[i + 1].SetActive(false);
-                    pages[i - 1].SetActive(true);
-                    pages[i - 2].SetActive(true);
-                    break;
-                }
-            }
+            ChangePage(currentPageNumber - 2);
         });
+
+        currentPageNumber = 0;
+        DisableAllPages();
+        SetPageActive(currentPageNumber, true);
+    }
+
+    private void DisableAllPages()
+    {
+        foreach (var page in pages)
+        {
+            page.SetActive(false);
+        }
+    }
+
+    private void ChangePage(int newPageNumber)
+    {
+        SetPageActive(currentPageNumber, false);
+        currentPageNumber = Mathf.Clamp(newPageNumber, 0, pages.Length - 1);
+        SetPageActive(currentPageNumber, true);
+    }
+
+    private void SetPageActive(int pageNumber, bool active)
+    {
+        pages[pageNumber].SetActive(active);
+        if (pageNumber % 2 == 0 && pageNumber + 1 < pages.Length)
+        {
+            pages[pageNumber + 1].SetActive(active);
+        }
+        else if (pageNumber % 2 != 0 && pageNumber - 1 >= 0)
+        {
+            pages[pageNumber - 1].SetActive(active);
+        }
     }
 }
