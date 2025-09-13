626584e5aead340d791c049cd6c3847d94ef8152
Hub1X <hubert.poteraj@gmail.com>
2025-09-13 19:41:16 +0200

    Upgrading Warning Design
    - Added Glow Animation
    - Added Show/Hide Animation (Not Working Yet)

--- Assets/Scripts/UI/HUD/DetectionWarningUI.cs
+++ Assets/Scripts/UI/HUD/DetectionWarningUI.cs
@@ -1,16 +1,26 @@
+using UnityEditor.Animations;
+using UnityEditor.Rendering;
 using UnityEngine;
 
 public class DetectionWarningUI : MonoBehaviour
 {
+    [SerializeField]
+    private Animator warningAnimator;
+
+    private const string Show = "Show";
+    private const string Hide = "Hide";
+
+
     private void Awake()
     {
         DetectionManager.DetectionOccured += () => SetWarningUIEnabled(true);
         DetectionManager.DetectionRemoved += () => SetWarningUIEnabled(false);
-        SetWarningUIEnabled(false);
+        //SetWarningUIEnabled(false);
     }
 
     private void SetWarningUIEnabled(bool enabled)
     {
-        gameObject.SetActive(enabled);
+        warningAnimator.SetTrigger(enabled ? Show : Hide);
+        //gameObject.SetActive(enabled);
     }
 }
