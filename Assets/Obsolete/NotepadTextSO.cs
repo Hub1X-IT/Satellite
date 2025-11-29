3babb214f2c55d1350f430220c2bca4036736ba4
Widzew12 <szmnglowacz@gmail.com>
2025-11-29 22:08:16 +0100

    Delete obsolete objects

--- Assets/Obsolete/NotepadTextSO.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(menuName = "NotepadText")]
-public class NotepadTextSO : ScriptableObject
-{
-    [Tooltip("Text for the notepad")]
-    public string textboxText;
-}
--- Assets/Obsolete/TextClipboard.cs
+++
@@ -1,6 +0,0 @@
-public class TextClipboard
-{
-    public static string DisplayedText { get; set; } = string.Empty;
-
-    public static string CopiedText { get; set; } = string.Empty;
-}
